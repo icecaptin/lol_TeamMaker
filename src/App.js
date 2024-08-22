7bdf642176261ef08b6b029f130b72de4d565eca
IceCaptin <tjdqn0228@gmail.com>
2024-08-22 23:07:07 +0900

    20240822_오랜만에커밋2

--- src/App.js
+++ src/App.js
@@ -22,32 +22,6 @@ function App() {
   const [team1, setTeam1] = useState([]);
   const [team2, setTeam2] = useState([]);
   const [duplicateMessage, setDuplicateMessage] = useState('');
-  const [serverTime, setServerTime] = useState('');
-
-  // useEffect(() => {
-  //   const fetchServerTime = async () => {
-  //     try {
-  //       const response = await axios.get('https://www.ticketlink.co.kr', {
-  //         headers: {
-  //           'Content-Type': 'text/html',
-  //         },
-  //       });
-  //       const doc = parser.parseFromString(response.data, 'text/html');
-  //       const timeElement = doc.querySelector('#server-time');
-
-  //       if (timeElement) {
-  //         setServerTime(timeElement.textContent);
-  //       } else {
-  //         console.error('서버 시간 정보를 찾을 수 없습니다.');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching server time:', error);
-  //     }
-  //   };
-
-  //   fetchServerTime();
-  // }, []);
-
 
   const handleTextFieldChange = (event, index) => {
     const newMembersText = [...membersText];
@@ -72,16 +46,18 @@ function App() {
 
   const shuffleTeamsWithConstraints = () => {
     let validTeams = false;
+  
     while (!validTeams) {
       const membersArray = membersText.filter(member => member.trim() !== '');
-
-      const shuffleMembers = membersArray.sort(() => Math.random() - 0.5);
-      const middleIndex = Math.ceil(shuffleMembers.length / 2);
-
-      const team1 = shuffleMembers.slice(0, middleIndex);
-      const team2 = shuffleMembers.slice(middleIndex);
-
-      if (!team1.includes('귤') || !team1.includes('브레이커')) {
+      const shuffledMembers = membersArray.sort(() => Math.random() - 0.5);
+      const middleIndex = Math.ceil(shuffledMembers.length / 2);
+  
+      const team1 = shuffledMembers.slice(0, middleIndex);
+      const team2 = shuffledMembers.slice(middleIndex);
+      const isGulInTeam1 = team1.includes('귤');
+      const isBreakerInTeam1 = team1.includes('브레이커');
+  
+      if (!(isGulInTeam1 && isBreakerInTeam1)) {
         setTeam1(team1);
         setTeam2(team2);
         validTeams = true;
@@ -132,11 +108,6 @@ function App() {
             {duplicateMessage && <h2 style={{ color: 'red', marginLeft: '10px' }}>{duplicateMessage}</h2>}
           </Box>
         </Box>
-      </Box>
-
-      <Box>
-        <h1>현재 Ticketlink 서버 시간:</h1>
-        <p>{serverTime}</p>
       </Box>
     </>
   );
